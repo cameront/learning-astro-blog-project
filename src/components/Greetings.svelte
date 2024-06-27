7b396afdee3911f15629c2201c34c2c8c067a0fa
Cameron Tangney <cameron.tangney@gmail.com>
2024-06-27 16:18:04 -0700

    finished up the tutorial. yay

---
+++ src/components/Greetings.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let messages: string[] = [];
+  let greeting = "";
+  function setGreeting() {
+    greeting = messages[Math.floor(Math.random() * messages.length)];
+  }
+</script>
+
+<div>
+  <h3>{greeting}! Thank you for visiting!</h3>
+  <button on:click={setGreeting}> New Greeting </button>
+</div>
